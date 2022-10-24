7f22dd61bfe244ef330121c2e2e649c2892fff98
panagiotis <panos.kontoeidis@gmail.com>
2022-10-24 14:43:46 +0300

    changes

--- CMakeFiles/test.dir/DependInfo.cmake
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -1,14 +1,22 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/panagiotis/Desktop/Project-Emiris/test.cpp" "/home/panagiotis/Desktop/Project-Emiris/CMakeFiles/test.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "CGAL_USE_CORE=1"
+  "CGAL_USE_GMPXX=1"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/stelios/Desktop/PROJECT/test.cpp" "CMakeFiles/test.dir/test.cpp.o" "gcc" "CMakeFiles/test.dir/test.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # Targets to which this target links.
